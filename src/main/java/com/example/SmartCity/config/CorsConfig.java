928d77228c5bc0a761f9e8b47756345f63523945
sandhya <rameshsandhya280@gmail.com>
2026-06-02 15:51:18 +0530

    replacing frontend live link with localhost

--- src/main/java/com/example/SmartCity/config/CorsConfig.java
+++ src/main/java/com/example/SmartCity/config/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:3000") // 👈 explicit origin
+                .allowedOrigins("http://smartcity-ochre.vercel.app") // 👈 explicit origin
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*");
 
