172d6373992abf5d5cb0ee4d42e9c601ba1bfd61
sandhya <rameshsandhya280@gmail.com>
2026-06-02 15:56:32 +0530

    protocol changing for url http to https

--- src/main/java/com/example/SmartCity/config/SecurityConfig.java
+++ src/main/java/com/example/SmartCity/config/SecurityConfig.java
@@ -31,7 +31,7 @@
         public CorsConfigurationSource corsConfigurationSource() {
 
             CorsConfiguration configuration = new CorsConfiguration();
-            configuration.setAllowedOrigins(List.of("http://smartcity-ochre.vercel.app"));
+            configuration.setAllowedOrigins(List.of("https://smartcity-ochre.vercel.app"));
             configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
             configuration.setAllowedHeaders(List.of("*"));
             configuration.setAllowCredentials(true);
